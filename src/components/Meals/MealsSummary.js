0e49d95aee757849eed8fac5b318a63d3c5b606d
Ophir Bentov <wannabegurux@gmail.com>
2023-08-01 15:40:18 -0700

    added a meals component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+import classes from '.MealsSummary.module.css'
+const MealsSummary = () => {
+    return <section className={classes.summary}>
+        <h2>Delicious Food, Delivered to You</h2>
+        <p>
+            Choose your favorite meal from a broad selecion, enjoy it at home.
+        </p>
+        <p>
+            All our meals are cooked with the best ingredients, by experienced chefs.
+        </p>
+    </section>
+};
+
+export default MealsSummary;
