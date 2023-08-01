cde1a1fb1d56014c8b0d1d71bde7fbdb311fa286
Ophir Bentov <wannabegurux@gmail.com>
2023-08-01 15:19:15 -0700

    completed header

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,6 +1,15 @@
-const HeaderCartButton = props => {
-    return <button>
-        <span></span><span></span><span></span>
+import CartIcon from "../Cart/CartIcon";
+import classes from './HeaderCart.module.css'
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
     </button>
+  );
 };
 export default HeaderCartButton;
