3a4e1a5876dba4027dc2e9c2b9ccf50f7f7f5d73
Ophir Bentov <wannabegurux@gmail.com>
2023-08-01 15:42:42 -0700

    completed meals component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,10 @@
+import AvailableMeals from "./AvailableMeals.Js";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+    return <>
+        <MealsSummary/>
+        <AvailableMeals/>
+    </>
+};
+export default Meals
