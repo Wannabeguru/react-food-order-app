1cf7a4bf8f83d647a9d50f5c395181f64a000c1e
Ophir Bentov <wannabegurux@gmail.com>
2023-08-02 14:40:17 -0700

    added functionality to close button, clicking on backdrop to close modal, and opening modal by clicking on cart

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,7 +1,7 @@
 import classes from "./Modal.module.css";
 import ReactDOM from "react-dom";
 const BackDrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -17,7 +17,7 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <>
-      {ReactDOM.createPortal(<BackDrop />, portalElement)}
+      {ReactDOM.createPortal(<BackDrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
